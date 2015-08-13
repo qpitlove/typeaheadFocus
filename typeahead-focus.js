df4f57211bf0152b5b721f94e42548fb9695bfb5
김정기/협업시스템개발팀/NE <kim.jeongki@nhnent.com>
2015-08-13 21:43:34 +0900

    empty key naming change -> emptyKeyForAllList fix jshint error

--- typeahead-focus.js
+++ typeahead-focus.js
@@ -14,35 +14,35 @@ angular.module('typeahead-focus', [])
     return {
       require: 'ngModel',
       link: function (scope, element, attr, ngModel) {
+        var emptyKeyForAllList = '[$empty$]';
 
         //trigger the popup on 'click' because 'focus'
         //is also triggered after the item selection
         element.bind('focus', function () {
-          var secretEmptyKey = '[$empty$]';
 
           var viewValue = ngModel.$viewValue;
 
           //restore to null value so that the typeahead can detect a change
-          if (ngModel.$viewValue == secretEmptyKey) {
+          if (ngModel.$viewValue === emptyKeyForAllList) {
             ngModel.$setViewValue(null);
           }
 
           //force trigger the popup
-          ngModel.$setViewValue(secretEmptyKey);
+          ngModel.$setViewValue(emptyKeyForAllList);
 
           //set the actual value in case there was already a value in the input
-          ngModel.$setViewValue(viewValue || secretEmptyKey);
+          ngModel.$setViewValue(viewValue || emptyKeyForAllList);
         });
 
         scope.$watch(function(){
           return ngModel.$viewValue;
         }, function(newVal){
-          ngModel.$setViewValue(newVal || secretEmptyKey);
+          ngModel.$setViewValue(newVal || emptyKeyForAllList);
         });
 
         //compare function that treats the empty space as a match
         scope.$emptyOrMatch = function (actual, expected) {
-          if (expected == secretEmptyKey) {
+          if (expected === emptyKeyForAllList) {
             return true;
           }
           return actual.toLowerCase().indexOf(expected.toLowerCase()) > -1;
